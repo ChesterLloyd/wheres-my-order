9728e7f18d20d09f136f31d2963f4e237e82eb46
Chester Lloyd <contact@chester-lloyd.co.uk>
2025-06-06 20:27:16 +0100

    Added recent purchases to the dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Purchase;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        $ongoingOrders = [];
-        $previousOrders = [];
+        $ongoingOrders = $entityManager->getRepository(Purchase::class)
+            ->findRecentOngoingPurchasesByUserId($this->getUser()->getId());
+        $previousOrders = $entityManager->getRepository(Purchase::class)
+            ->findRecentCompletedPurchasesByUserId($this->getUser()->getId());
 
         return $this->render('dashboard/index.html.twig', [
             'active' => 'dashboard',
--- src/Repository/PurchaseRepository.php
+++ src/Repository/PurchaseRepository.php
@@ -16,28 +16,46 @@ class PurchaseRepository extends ServiceEntityRepository
         parent::__construct($registry, Purchase::class);
     }
 
-    //    /**
-    //     * @return Order[] Returns an array of Order objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('o')
-    //            ->andWhere('o.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('o.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * @return Purchase[] Returns an array of purchases that are
+     * ongoing for a specific user.
+     */
+    public function findRecentOngoingPurchasesByUserId(int $userId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.status IN (:statuses)')
+            ->andWhere('p.user = :user')
+            ->setParameter('statuses', [
+                Purchase::STATUS_ACKNOWLEDGED,
+                Purchase::STATUS_DISPATCHED,
+                Purchase::STATUS_OUT_FOR_DELIVERY,
+            ])
+            ->setParameter('user', $userId)
+            ->orderBy('p.orderDate', 'DESC')
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
-    //    public function findOneBySomeField($value): ?Order
-    //    {
-    //        return $this->createQueryBuilder('o')
-    //            ->andWhere('o.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * @return Purchase[] Returns an array of purchases that have
+     * completed for a specific user.
+     */
+    public function findRecentCompletedPurchasesByUserId(int $userId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.status IN (:statuses)')
+            ->andWhere('p.user = :user')
+            ->setParameter('statuses', [
+                Purchase::STATUS_CANCELLED,
+                Purchase::STATUS_DELIVERED,
+            ])
+            ->setParameter('user', $userId)
+            ->orderBy('p.orderDate', 'DESC')
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
